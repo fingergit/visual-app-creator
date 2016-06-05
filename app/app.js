4c20c407f11b70fac572c8d7b0769f626e3e132d
fingergit <fingergit@sina.com>
2016-06-05 11:57:04 +0800

    使用ui-router替换原来的route

--- app/app.js
+++ app/app.js
@@ -2,13 +2,39 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ui.router'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+  // $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  // $routeProvider.otherwise({redirectTo: '/view1'});
+
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+      .state('state1', {
+        url: "/state1",
+        templateUrl: "state1/state1.html"
+      })
+      .state('state1.list', {
+        url: "/list",
+        templateUrl: "state1/state1.list.html",
+        controller: function($scope) {
+          $scope.items = ["A", "List", "Of", "Items"];
+        }
+      })
+      .state('state2', {
+        url: "/state2",
+        templateUrl: "state2/state2.html"
+      })
+      .state('state2.list', {
+        url: "/list",
+        templateUrl: "state2/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      });
 }]);
